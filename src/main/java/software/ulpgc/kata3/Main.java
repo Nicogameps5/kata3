a74d05ba965c5f865c874888f650289dc781b53a
Nico <jkjkloljbjk@gmail.com>
2024-10-28 12:50:16 +0000

    first commit

---
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -0,0 +1,23 @@
+package software.ulpgc.kata3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:/Descargas/title.basics.tsv");
+        List<Title> titles = new FileTitleLoader(file, new TsvTitleDeserializer()).load();
+        Map<Integer, Integer> stats = new HashMap<>();
+        for (Title title : titles) {
+            stats.put(title.getYear(), stats.getOrDefault(title.getYear(), 0) + 1);
+        }
+
+        for (int year : stats.keySet()) {
+            System.out.println(year + "\t" + stats.get(year));
+        }
+
+    }
+}
